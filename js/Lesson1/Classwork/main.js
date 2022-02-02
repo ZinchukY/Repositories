e94806c2782e1c1dfa197f87326115c28a13504f
ZinchukY <04042002ura@gmail.com>
2022-02-02 11:44:20 +0200

    Initial commit

---
+++ js/Lesson1/Classwork/main.js
@@ -0,0 +1,79 @@
+//1
+let n = parseInt(Math.random()*10000) ;
+
+let n1 = parseInt(Math.random()*10000) ;
+
+let n2 = parseInt(Math.random()*10000) ;
+
+let n3 = parseInt(Math.random()*10000) ;
+
+let n4 = parseInt(Math.random()*10000) ;
+
+let n5 = parseInt(Math.random()*10000) ;
+
+let n6 = parseInt(Math.random()*10000) ;
+
+let n7 = parseInt(Math.random()*10000) ;
+
+let n8 = parseInt(Math.random()*10000) ;
+
+let n9 = parseInt(Math.random()*10000) ;
+
+ let massive = [n, n1, n2, n3, n4, n5, n6, n7, n8, n9];
+ console.log(massive)
+ let result = n + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9;
+ console.log(result)
+
+
+
+//2-4
+let book = {
+    book1: {
+         name:'Think and grow rich',
+         pages_number:'238',
+         genre: 'Self-help book',
+         author: 'Napoleon hill'
+    },
+
+    book2:{
+        name: 'The 7 Habits of Highly Effective People',
+        pages_number: '381',
+        genre: 'Nonfiction business',
+        author:'Steven Covey'
+    }
+
+}
+console.log(book.book1)
+console.log(book.book2)
+
+
+//5
+let h=23;
+
+let w=10;
+
+let s = h * w;
+
+console.log(s)
+
+
+//6
+let heightC=10;
+
+let dC=4;
+
+let pi=3.14;
+
+let v = pi * dC * heightC;
+
+console.log(v)
+
+
+//7
+let l=3;
+
+let m =4;
+
+let k =Math.sqrt(Math.pow(m,2) + Math.pow(l,2));
+
+console.log(k)
---
+++ js/Lesson1/Homework/main.js
@@ -0,0 +1,47 @@
+//1
+let h = 'hello';
+let o = 'owu';
+let com = 'com';
+let u = 'ua';
+let f = '1';
+let t = '10';
+let m = '-999';
+let ft = '123';
+let pi = '3.14'
+let ts = '2.7';
+let s = '16';
+let tr = 'true';
+let fl = 'false';
+
+//2
+console.log(h);
+console.log(o);
+console.log(com);
+console.log(u);
+console.log(f);
+console.log(t);
+console.log(m);
+console.log(ft);
+console.log(pi);
+console.log(ts);
+console.log(s);
+console.log(tr);
+console.log(fl);
+
+// alert(h);
+// alert(o);
+// alert(com);
+// alert(u);
+// alert(f);
+// alert(t);
+// alert(m);
+// alert(ft);
+// alert(pi);
+// alert(ts);
+// alert(s);
+// alert(tr);
+// alert(fl);
+
+document.write(h, o, com, u, f, t, m, ft, pi, ts, s, tr, fl);
+
+//3
