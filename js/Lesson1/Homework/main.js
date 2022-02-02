510fcfadc3e843f9b5586a2985da6bb6d67e46ec
ZinchukY <04042002ura@gmail.com>
2022-02-02 15:53:36 +0200

    homework done

--- js/Lesson1/Homework/main.js
+++ js/Lesson1/Homework/main.js
@@ -1,17 +1,17 @@
 //1
-let h = 'hello';
-let o = 'owu';
-let com = 'com';
-let u = 'ua';
-let f = '1';
-let t = '10';
-let m = '-999';
-let ft = '123';
-let pi = '3.14'
-let ts = '2.7';
-let s = '16';
-let tr = 'true';
-let fl = 'false';
+var h = 'hello';
+var o = 'owu';
+var com = 'com';
+var u = 'ua';
+var f = '1';
+var t = '10';
+var m = '-999';
+var ft = '123';
+var pi = '3.14'
+var ts = '2.7';
+var s = '16';
+var tr = 'true';
+var fl = 'false';
 
 //2
 console.log(h);
@@ -45,3 +45,97 @@ console.log(fl);
 document.write(h, o, com, u, f, t, m, ft, pi, ts, s, tr, fl);
 
 //3
+var h = 'hi';
+var o = 'ow'
+var com = 'moc'
+var u = 'au'
+var f = '-1'
+var t = '-10'
+var m = '999'
+var ft = '321'
+var pi = '14.3'
+var ts = '7.2'
+var s = '61'
+var tr = 'false'
+var fl = 'true'
+
+//4
+console.log(h);
+console.log(o);
+console.log(com);
+console.log(u);
+console.log(f);
+console.log(t);
+console.log(m);
+console.log(ft);
+console.log(pi);
+console.log(ts);
+console.log(s);
+console.log(tr);
+console.log(fl);
+
+// alert(h);
+// alert(o);
+// alert(com);
+// alert(u);
+// alert(f);
+// alert(t);
+// alert(m);
+// alert(ft);
+// alert(pi);
+// alert(ts);
+// alert(s);
+// alert(tr);
+// alert(fl);
+
+document.write(h, o, com, u, f, t, m, ft, pi, ts, s, tr, fl);
+//5
+let firstName = 'Zinchuk';
+let middleName = 'Yurii';
+let lastName = 'Romanovich';
+let person = [firstName, middleName, lastName]
+
+//6
+// prompt(firstName);
+// prompt(middleName);
+// prompt(lastName);
+
+//7
+prompt("Вітаю " +middleName + " " + lastName + " . Тобі 20 років.");
+
+//8
+var a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a, b, c);
+
+//9
+console.log(5<6);
+
+console.log(5==6);
+
+console.log(5>6);
+
+console.log(5===6);
+
+console.log(10===10);
+
+console.log(10==10);
+
+console.log(10!==10);
+
+console.log(10>10);
+
+console.log(10<10);
+
+console.log(123 === '123');
+
+console.log(123 >= '123');
+
+let str = "20";
+var a = 5;
+document.write(str + a + "<br/>"); //205
+document.write(str - a + "<br/>"); //15
+document.write(str * "2" + "<br/>"); //40
+document.write(str / 2 + "<br/>"); //10
