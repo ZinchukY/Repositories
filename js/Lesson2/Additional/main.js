13026b789980412a5fe42169a3aa82b8a3feeb4b
ZinchukY <04042002ura@gmail.com>
2022-02-04 18:05:14 +0200

    addictional done

---
+++ js/Lesson2/Additional/main.js
@@ -0,0 +1,37 @@
+//1
+// let friends = ["Ivan", "Slavik", "Grisha", "Lera", "Ira"];
+// if (friends.length >=3 ){
+//     console.log('Це великий масив')
+// }else{
+//     console.log('це маленький масив')
+// }
+
+//2
+// let a = parseInt(prompt('Введіть число 1: '))
+// let b = parseInt(prompt('Введіть число 2: '))
+// let c = parseInt(prompt('Введіть число 3: '))
+//
+// if (a>b && a<c || a<b && a>c){
+//     console.log('Це число є середнім: ' + a)
+// }else if (b>a && b<c || b<a && b>c){
+//     console.log('Це число є середнім: ' + b)
+// }else {
+//     console.log('Це число є середнім: ' + c)
+// }
+
+//3
+// let a = parseInt(prompt('Введіть число 1: '))
+// let b = parseInt(prompt('Введіть число 2: '))
+// let result = ( a + b )<4 ?  'Мало' : 'Багато';
+// alert(result);
+
+
+//4
+let chosenNumber = parseInt(prompt('Введіть будь яке число будь яке число від -100 до +100: '));
+let number = (0 > chosenNumber && chosenNumber >=-100 ) ? 'Число негативне' :
+    (chosenNumber > 0 && chosenNumber <=100) ? 'Число позитивне' :
+        (chosenNumber === 0) ? 'Це число нуль':
+            'Ви ввели число яке не входить у вказаний діапазон';
+
+alert(number)
+
