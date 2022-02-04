c361d11c27cc3cdf70cfb5b99aee456db8c4f766
ZinchukY <04042002ura@gmail.com>
2022-02-04 16:06:56 +0200

    Homework done

---
+++ js/Lesson2/Homework/main.js
@@ -0,0 +1,83 @@
+//1
+// let time = parseInt(prompt('Введіть поточну хвилину від 0 до 59 '));
+//
+// if (time >= 0 && time <= 15){
+//     console.log('Перша чверть години')
+// }else if (time >= 16 && time <=29 ){
+//     console.log('Друга чверть години')
+// }else if ( time>= 30 && time <=44 ){
+//     console.log('Третя чверть години')
+// }else if ( time>= 45 && time <=59 ){
+//     console.log('Четверта чверть години')
+// }else{
+//     console.log("Ваші дані не попадають в годинні рамки")
+// }
+
+
+//2
+// let day = parseInt(prompt('Введіть день місяця від 1 до 31 '))
+// if (day>=1 && day <=10){
+//     console.log('Перша декада місяця')
+// }else if(day>= 11 && day<=20){
+//     console.log('Друга декада місяця')
+// }else if(day>=21 && day<=31){
+//     console.log('Третя декада місяця')
+// }else {
+//     console.log('Це виходить за рамки місяця')
+// }
+
+
+//3
+// let a = parseInt(prompt('Введіть число 1, 0, -3: '))
+// if (a !== 0){
+//     console.log('ВІРНО')
+// }else{
+//     console.log('НЕВІРНО')
+// }
+
+
+//4
+// let day = parseInt(prompt('Введіть день тижня: '))
+// switch (day){
+//     case 1:{
+//         console.log('Сходити в магаз')
+//     }break;
+//     case 2:{
+//         console.log('Сходити в зал')
+//     }break;
+//     case 3:{
+//         console.log('Почати читати книгу')
+//     }break;
+//     case 4:{
+//         console.log('Зустрітись з знайомими')
+//     }break;
+//     case 5:{
+//         console.log('Прибрати вдома')
+//     }break;
+//     case 6:{
+//         console.log('Сходити на відпочинок на природу')
+//     }break;
+//     case 7:{
+//         console.log('Нарубати дров')
+//     }break;
+//     default:
+//         console.log('Тиждень має 7 днів, ви ввели неіснуючий день')
+// }
+
+
+//5
+// let x = parseInt(prompt('Введіть рік: '))
+// if ( x%4===0 ){
+//     console.log('Це високосний рік')
+// }else{
+//     console.log('Це не високосний рік')
+// }
+
+
+//6
+// let jsname = prompt('Яка офіційна назва JavaScript ')
+// if (jsname === 'ECMAScript'){
+//     console.log('Правильно!')
+// }else{
+//     console.log('Не знаєте? ECMAScript!')
+// }
