67dbbc5963ec912363487e1cb42298d388ce5988
ZinchukY <04042002ura@gmail.com>
2022-08-03 19:57:37 +0300

    lesson js 2

---
+++ june2022/js/Lesson2/main.js
@@ -0,0 +1,142 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+// let array = [true, 'hello', 23, false, 'bye', 44, 'Vova', 29, 'boolean', 88]
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array[3]);
+// console.log(array[4]);
+// console.log(array[5]);
+// console.log(array[6]);
+// console.log(array[7]);
+// console.log(array[8]);
+// console.log(array[9]);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// const book1 = {
+//     title: 'Great',
+//     pageCount: 200,
+//     genre: 'Horror'
+// }
+// const book2 = {
+//     title: 'Amazing',
+//     pageCount: 250,
+//     genre: 'Thriller'
+// }
+// const book3 = {
+//     title: 'Future',
+//     pageCount: 350,
+//     genre: 'Fantasy'
+// }
+// console.log(book1, book2, book3)
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+// const book1 = {
+//     title: 'Great',
+//     pageCount: 200,
+//     genre: 'Horror',
+//     authors: [
+//         {
+//             name: 'James',
+//             age: 33
+//         },
+//         {
+//             name: 'Sasha',
+//             age: 45
+//         }
+//     ]
+// }
+// const book2 = {
+//     title: 'Amazing',
+//     pageCount: 250,
+//     genre: 'Thriller',
+//     authors: [
+//         {
+//             name: 'James',
+//             age: 33
+//         },
+//         {
+//             name: 'Sasha',
+//             age: 45
+//         }
+//     ]
+// }
+// const book3 = {
+//     title: 'Future',
+//     pageCount: 350,
+//     genre: 'Fantasy',
+//     authors: [
+//         {
+//             name: 'James',
+//             age: 33
+//         },
+//         {
+//             name: 'Sasha',
+//             age: 45
+//         }
+//     ]
+// }
+// console.log(book1, book2, book3)
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+// let array = [
+//     {
+//         name: 'Oleg',
+//         username: 'Olejik',
+//         password: '1234'
+//     },
+//     {
+//         name: 'Oleg',
+//         username: 'Olejik',
+//         password: '4321'
+//     },
+//     {
+//         name: 'Oleg',
+//         username: 'Olejik',
+//         password: '9876'
+//     },
+//     {
+//         name: 'Oleg',
+//         username: 'Olejik',
+//         password: '6789'
+//     },
+//     {
+//         name: 'Oleg',
+//         username: 'Olejik',
+//         password: 'qwer'
+//     },
+//     {
+//         name: 'Oleg',
+//         username: 'Olejik',
+//         password: 'lgfhl'
+//     },
+//     {
+//         name: 'Oleg',
+//         username: 'Olejik',
+//         password: 'oleg2022'
+//     },
+//     {
+//         name: 'Oleg',
+//         username: 'Olejik',
+//         password: 'oleg1999'
+//     },
+//     {
+//         name: 'Oleg',
+//         username: 'Olejik',
+//         password: 'krfok'
+//     },
+//     {
+//         name: 'Oleg',
+//         username: 'Olejik',
+//         password: 'bratan1'
+//     }
+// ]
+// console.log(array[0].password);
+// console.log(array[1].password);
+// console.log(array[2].password);
+// console.log(array[3].password);
+// console.log(array[4].password);
+// console.log(array[5].password);
+// console.log(array[6].password);
+// console.log(array[7].password);
+// console.log(array[8].password);
+// console.log(array[9].password);
