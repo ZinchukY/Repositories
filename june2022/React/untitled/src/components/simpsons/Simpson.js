577c8ea504569a094b6f62add6df73b03bce1ad4
ZinchukY <04042002ura@gmail.com>
2022-09-15 17:12:15 +0300

    hw1

---
+++ june2022/React/untitled/src/components/simpsons/Simpson.js
@@ -0,0 +1,12 @@
+function Simpson(props) {
+    const {simpson} = props;
+    return (
+        <div>
+            <h3>{simpson.name} -- {simpson.age}</h3>
+            <p>{simpson.surname} -- {simpson.info}</p>
+            <img src={simpson.photo} alt={simpson.name}/>
+        </div>
+    );
+}
+
+export default Simpson;
