522ccde812bb3e152e8995e55e4250faa576b166
ZinchukY <04042002ura@gmail.com>
2022-09-17 19:58:30 +0300

    hw1.2

---
+++ june2022/React/untitled/src/components/person/Character.js
@@ -0,0 +1,13 @@
+
+function Character(props) {
+    const {character} = props;
+
+    return (
+        <div>
+            <h3>{character.id} -- {character.name}</h3>
+            <p>{character.status}</p>
+        </div>
+    );
+}
+
+export default Character;
